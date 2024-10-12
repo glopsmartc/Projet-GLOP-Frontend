8a3d056dc62f469322891715e2c9fde6892627bf
Nadia Amesrouy <nadia.amesrouy.etu@univ-lille.fr>
2024-10-12 02:02:13 +0200

    forgot_pass

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { ForgotPassComponent } from './forgot-pass/forgot-pass.component';
 
 export const routes: Routes = [
-    {path: 'login', component: LoginPageComponent},
-  ];
+  { path: 'login', component: LoginPageComponent },
+  { path: 'forgot-password', component: ForgotPassComponent },
+];
