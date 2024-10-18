c92ca22bfe20682f5ac431288922064621b476cf
Nadia Amesrouy <nadia.amesrouy.etu@univ-lille.fr>
2024-10-18 17:58:01 +0200

    rtrlink

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { ForgotPassComponent } from './forgot-pass/forgot-pass.component';
+import { SubscriptionFormComponent } from './subscription-form/subscription-form.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginPageComponent },
   { path: 'forgot-password', component: ForgotPassComponent },
+  { path: 'subscription-form', component: SubscriptionFormComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
