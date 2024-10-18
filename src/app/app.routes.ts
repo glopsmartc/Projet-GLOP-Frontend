4534d3cc19ef959236efa3fab9815f174baab807
Nadia Amesrouy <nadia.amesrouy.etu@univ-lille.fr>
2024-10-18 22:58:18 +0200

    routesmdfd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { ForgotPassComponent } from './forgot-pass/forgot-pass.component';
 import { SubscriptionFormComponent } from './subscription-form/subscription-form.component';
+import { SidebarClientComponent } from './sidebar-client/sidebar-client.component';
 
 
 import { ReactiveFormsModule } from '@angular/forms';
@@ -11,6 +12,7 @@ export const routes: Routes = [
   { path: 'login', component: LoginPageComponent },
   { path: 'forgot-password', component: ForgotPassComponent },
   { path: 'subscription-form', component: SubscriptionFormComponent },
+  { path: 'sidebar-test', component: SidebarClientComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
 
