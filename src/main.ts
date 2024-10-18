fffce0f6f6591dee06f34dcc9e9a732c97f6e906
salmazendah <zendah91@gmail.com>
2024-10-19 01:39:33 +0200

    Sauvegarde des modifications avant mise à jour d'Angular

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { SubscriptionFormComponent } from './app/subscription-form/subscription-form.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
