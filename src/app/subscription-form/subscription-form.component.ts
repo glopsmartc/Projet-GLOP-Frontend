f9fb6c663cb190613e6cc02e7eda9266b33559bb
salmazendah <zendah91@gmail.com>
2024-10-27 21:41:57 +0100

    importing external API for country selection

--- src/app/subscription-form/subscription-form.component.ts
+++ src/app/subscription-form/subscription-form.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-subscription-form',
@@ -12,13 +13,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 })
 export class SubscriptionFormComponent {
   myForm: FormGroup;
+  countries: string[] = []; // Liste de pays pour le champ destination
 
   showAssurerTransport = false;
   showAssurerPersonnes = false;
   showDebutContrat = false;
-  showDateAllerRetour = false;
+  showDateAllerRetourDestination = false;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private http: HttpClient) {
     this.myForm = this.fb.group({
       assurerTransport: [false],
       voiture: [false],
@@ -31,29 +33,40 @@ export class SubscriptionFormComponent {
       debutContrat: ['', Validators.required],
       dateAller: [''],
       dateRetour: [''],
+      destination: [''],
     });
       // Écoute les changements sur "dureeContrat"
       this.myForm.get('dureeContrat')?.valueChanges.subscribe(value => {
         this.updateCheckboxVisibility(value);
       });
+      this.loadCountries();//pour charger la liste des pays dès que le composant est initialisé
     }
+
+    loadCountries() {
+      const url = 'https://restcountries.com/v3.1/all';
+      this.http.get<any[]>(url).subscribe((data) => {
+        this.countries = data.map(country => country.name.common);
+        this.countries.sort(); 
+      });
+    }
+
     updateCheckboxVisibility(duree: string) {
       // Réinitialise les champs de choix
       this.myForm.get('assurerTransport')?.setValue(false);
       this.myForm.get('assurerPersonnes')?.setValue(false);
+      this.myForm.get('destination')?.setValue('');
   
       // Affiche les options selon la durée choisie
       if (duree === '1_voyage') {
         this.showAssurerTransport = false;
         this.showAssurerPersonnes = true;
         this.showDebutContrat = false;
-        this.showDateAllerRetour = true;
-
+        this.showDateAllerRetourDestination= true;
       } else {
         this.showAssurerTransport = true;
         this.showAssurerPersonnes = true;
         this.showDebutContrat = true;
-        this.showDateAllerRetour = false;
+        this.showDateAllerRetourDestination = false;
       }
     }
   
