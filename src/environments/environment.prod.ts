13ceed1a40ce9ee683f8d224483ec2b6692fdd45
rawanghosn <rawanghosn22@gmail.com>
2024-12-08 15:02:34 +0100

    nouvelles methodes pour ocntrat

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiBaseUrl: 'http://172.28.100.191:80' // Production API URL
+    apiBaseUrl: 'http://172.28.100.191:80', // Production API URL
+    apiBaseUrlContrat: 'http://172.28.100.191:8082'
   };
