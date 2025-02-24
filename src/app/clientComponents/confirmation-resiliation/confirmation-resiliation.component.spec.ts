2394af3375ff16b277897710862f3d15f679d55b
BouchraHayani <bouchra.hayani.etu@univ-lille.fr>
2025-02-24 08:51:37 +0100

    Update confirmation-resiliation.component.spec.ts
    CommitTestConfirmationResiliationComponent

--- src/app/clientComponents/confirmation-resiliation/confirmation-resiliation.component.spec.ts
+++ src/app/clientComponents/confirmation-resiliation/confirmation-resiliation.component.spec.ts
@@ -1,16 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ConfirmationResiliationComponent } from './confirmation-resiliation.component';
+import { MatDialogRef } from '@angular/material/dialog';
 
 describe('ConfirmationResiliationComponent', () => {
   let component: ConfirmationResiliationComponent;
   let fixture: ComponentFixture<ConfirmationResiliationComponent>;
+  let dialogRef: jasmine.SpyObj<MatDialogRef<ConfirmationResiliationComponent>>;
 
   beforeEach(async () => {
+    const dialogRefSpy = jasmine.createSpyObj('MatDialogRef', ['close']);
+
     await TestBed.configureTestingModule({
-      imports: [ConfirmationResiliationComponent]
-    })
-    .compileComponents();
+      imports: [ConfirmationResiliationComponent],
+      providers: [
+        { provide: MatDialogRef, useValue: dialogRefSpy },
+      ],
+    }).compileComponents();
+
+    dialogRef = TestBed.inject(MatDialogRef) as jasmine.SpyObj<MatDialogRef<ConfirmationResiliationComponent>>;
 
     fixture = TestBed.createComponent(ConfirmationResiliationComponent);
     component = fixture.componentInstance;
@@ -19,5 +26,17 @@ describe('ConfirmationResiliationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should close dialog with "true" when onConfirm is called', () => {
+    component.onConfirm();
+
+    expect(dialogRef.close).toHaveBeenCalledWith(true);
+  });
+
+  it('should close dialog with "false" when onCancel is called', () => {
+    component.onCancel();
+
+    expect(dialogRef.close).toHaveBeenCalledWith(false);
   });
 });
